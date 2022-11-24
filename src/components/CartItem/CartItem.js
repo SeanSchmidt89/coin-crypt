a78a915b36a9046f54a7e42fd1126f7aec610f78
sean <seanschmidt89@yahoo.com>
2022-11-24 04:52:34 -0800

    complete redo for cart route, JSX and CSS

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { cartSliceActions } from "../../store/cartSlice";
+import { Link } from "react-router-dom";
 import "./CartItem.css";
 
 const CartItem = ({ item }) => {
@@ -21,10 +22,13 @@ const CartItem = ({ item }) => {
   return (
     <div className="cart-item">
       <h2>
-        {item.id.toUpperCase()}
+        <Link className="coin-title" to={`/coin/${item.id}`}>
+          {item.id.toUpperCase()}
+        </Link>
         <span className="market-cap-rank">#{item.marketCapRank}</span>
       </h2>
       <div className="lower">
+        {/* LEFT SIDE OF LOWER ITEM */}
         <div className="left">
           <p>{item.symbol.toUpperCase()}</p>
           <p>Price: ${item.price}</p>
@@ -32,8 +36,11 @@ const CartItem = ({ item }) => {
           <p>Total: ${item.totalCost.toLocaleString()}</p>
           <button onClick={removeItemHandler}>Remove From Cart</button>
         </div>
+        {/* RIGHT SIDE OF LOWER ITEM */}
         <div className="right">
-          <img src={item.image} alt={item.id} />
+          <Link to={`/coin/${item.id}`}>
+            <img src={item.image} alt={item.id} />
+          </Link>
           <div className="add-sub-btns">
             <button onClick={decreaseQuantityHandler}>-</button>
             <p>{item.quantity}</p>
--- src/routes/Cart/Cart.js
+++ src/routes/Cart/Cart.js
@@ -5,10 +5,29 @@ import "./Cart.css";
 
 const Cart = () => {
   const cartList = useSelector((state) => state.cart.items);
+  const cartTotalPrice = useSelector((state) => state.cart.totalPrice)
+  const cartTotalItems = useSelector((state) => state.cart.totalQuantity)
   return (
     <div className="cart">
-      {cartList.length > 0 &&
-        cartList.map((item) => <CartItem key={item.id} item={item} />)}
+      <h1>Shopping Cart</h1>
+      <div className="cart-container">
+        <div className="cart-list">
+          {cartList.length > 0 &&
+            cartList.map((item) => <CartItem key={item.id} item={item} />)}
+        </div>
+        <div className="cart-info">
+          {cartList.length > 0 ? (
+            <>
+              <h2 className="info-header">Cart Info</h2>
+              <p>Specific Coins: <span>{cartList.length}</span></p>
+              <p>Total Cart Items: <span>{cartTotalItems}</span></p>
+              <p>Cart Total (USD): <span>${cartTotalPrice.toLocaleString()}</span></p>
+            </>
+          ) : (
+            <h2>Your cart is empty</h2>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
