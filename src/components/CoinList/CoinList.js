1482c182ddb0715aa4566fa1eb2eb0d371c53452
sean <seanschmidt89@yahoo.com>
2022-11-25 07:05:03 -0800

    slice logic for home page to go through pages

--- src/components/CoinList/CoinList.js
+++ src/components/CoinList/CoinList.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { coinSliceActions } from "../../store/coinSlice";
 import Coin from "../Coin/Coin";
@@ -7,12 +7,13 @@ import "./CoinList.css";
 
 const CoinList = () => {
   const dispatch = useDispatch();
-  const coins = useSelector((state) => state.coins.coins);
+  const [pageNumber, setPageNumber] = useState(1);
+  const coins = useSelector((state) => state.coins.pageList);
 
   useEffect(() => {
     axios
       .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=12&page=1&sparkline=false"
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=200&page=1&sparkline=false"
       )
       .then((response) => dispatch(coinSliceActions.fetchCoins(response.data)))
       .catch((error) => {
@@ -20,20 +21,38 @@ const CoinList = () => {
       });
   }, [dispatch]);
 
-  useEffect(() => {
-    console.log(coins);
-  }, [coins]);
+  const nextPageHandler = (e) => {
+    if (pageNumber === 16) {
+      return;
+    } else {
+      setPageNumber(pageNumber + 1);
+      dispatch(coinSliceActions.nextPage(pageNumber));
+    }
+  };
+
+  const prevPageHandler = (e) => {
+    if(pageNumber === 1){
+      return
+    } else {
+      setPageNumber(pageNumber - 1);
+      dispatch(coinSliceActions.prevPage(pageNumber))
+    }
+    
+  };
   return (
     <div className="coin-list">
       <div className="coin-container">
         <div className="coin-grid">
           {coins.length > 0 &&
-            coins.map((item) => <Coin key={item.id} item={item} />)}
+            coins
+              .slice(0, 12)
+              .map((item) => <Coin key={item.id} item={item} />)}
         </div>
         <div className="grid-controls">
           <h2>Buttons</h2>
-          <button>add button</button>
-          <button>add button</button>
+          <button onClick={nextPageHandler}>next page</button>
+          <button onClick={prevPageHandler}>prev page</button>
+          {pageNumber}
           <button>add button</button>
           <button>add button</button>
           <button>add button</button>
--- src/store/coinSlice.js
+++ src/store/coinSlice.js
@@ -3,6 +3,9 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   coins: [],
   coinDetail: {},
+  pageList: [],
+  pageIndex1: 0,
+  pageIndex2: 12,
 };
 
 const coinSlice = createSlice({
@@ -11,10 +14,21 @@ const coinSlice = createSlice({
   reducers: {
     fetchCoins: (state, action) => {
       state.coins = action.payload;
+      state.pageList = state.coins.slice(0, 12);
     },
     coinDetail: (state, action) => {
-      state.coinDetail = action.payload
-    }
+      state.coinDetail = action.payload;
+    },
+    nextPage: (state, action) => {
+      state.pageIndex1 = action.payload * 12;
+      state.pageIndex2 = action.payload * 12 + 12;
+      state.pageList = state.coins.slice(state.pageIndex1, state.pageIndex2);
+    },
+    prevPage: (state, action) => {
+      state.pageIndex1 = action.payload * 12 - 24
+      state.pageIndex2 = action.payload * 12 - 12;
+      state.pageList = state.coins.slice(state.pageIndex1, state.pageIndex2);
+    },
   },
 });
 
