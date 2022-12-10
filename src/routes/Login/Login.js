774d934e303d75b5dfca2ec193044580f4fa7ed1
sean <seanschmidt89@yahoo.com>
2022-12-10 08:43:52 -0800

    adding form to logiin page

--- src/routes/Login/Login.js
+++ src/routes/Login/Login.js
@@ -2,7 +2,19 @@ import React from "react";
 import "./Login.css";
 
 const Login = () => {
-  return <div>Login</div>;
+  const submitHandler = (e) => {
+    e.preventDefault()
+  }
+  return (
+    <div className="login">
+      <h3>Login</h3>
+      <form className="login-form" onSubmit={submitHandler}>
+        <input placeholder="username" />
+        <input placeholder="password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
