ac9db88a73c960e3119c5a562fc28cb8900d7d04
sean <seanschmidt89@yahoo.com>
2022-12-26 00:44:21 -0800

    fixed side nav issues and css

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -16,38 +16,30 @@ const Nav = () => {
       <Link to="/">
         <h1>COIN CRYPT</h1>
       </Link>
-      <div className={!sideNavActive ? "nav-btns" : "side-nav-btns"}>
-        <Link to="/">Home</Link>
-        <Link to="/profile">Profile</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/sign-up">SignUp</Link>
-        <Link to="/cart" className="cart-link">
+      <div className={!sideNavActive ? "nav-btns" : "show-side-nav"}>
+        <Link to="/" onClick={sideNavHandler}>
+          Home
+        </Link>
+        <Link to="/profile" onClick={sideNavHandler}>
+          Profile
+        </Link>
+        <Link to="/login" onClick={sideNavHandler}>
+          Login
+        </Link>
+        <Link to="/sign-up" onClick={sideNavHandler}>
+          SignUp
+        </Link>
+        <Link to="/cart" onClick={sideNavHandler} className="cart-link">
           Cart {cartNumber ? cartNumber : null}
         </Link>
-
-        <div
-          className={sideNavActive ? "show-side-nav" : "hide-side-nav"}
-        ></div>
       </div>
       {!sideNavActive ? (
         <FaBars onClick={sideNavHandler} className="hamburger" size={20} />
       ) : (
-        <FaTimes onClick={sideNavHandler} className="x" size={25} />
+        <FaTimes onClick={sideNavHandler} className="x" size={20} />
       )}
     </div>
   );
 };
 
 export default Nav;
-
-function zipIt(women, men) {
-  if (women.length !== men.length) {
-    return "sizes don't match";
-  }
-  let newArr = [];
-  for (let i = 0; i < women.length; i++) {
-    newArr.push([women[i], men[i]]);
-  }
-  return newArr;
-}
-zipIt(["Ana", "Amy", "Lisa"], ["Bob", "Josh", "Tim"]);
